6653d64a53a76af7205c0d9d20652c7d8738ba18
nakte <ankita.joshi1591@gmail.com>
2016-05-03 17:31:22 -0400

    clean up

---
+++ readjson.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import codecs
+import re
+import json
+from string import punctuation
+from pprint import pprint
+
+
+data = open("input/tweets1.json").read()
+tdata = json.loads(data)
+
+for i in range(0, len(tdata)):
+	result1 = tdata[i]["tweet_text"]
+	#removing the URLs
+	result2 = re.sub(r"http\S+", "", result1)
+	#removing the punctuation marks
+        result3 = ''.join(c for c in result2 if c not in punctuation)	
+	#remove other non-ascii stuff
+	result4 = re.sub(r'[^\x00-\x7f]',r'', result3)
+	#remove extra spaces or lines
+	result5 = re.sub('\s+', ' ',result4).strip()
+	#remove "RT"
+	result6 = re.sub("RT",'',result5).strip()
+	#make the final string 	
+	line = tdata[i]["user_id"] +","+result6+"\n"
+	#write the output #id,tweet to the file
+	with codecs.open('file.txt', 'a', encoding = 'utf-8') as out:
+		out.write(line)
+		out.close
