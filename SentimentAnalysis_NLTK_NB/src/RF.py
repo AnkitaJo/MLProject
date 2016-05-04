926f4ee09b49d9a21c0341d0eeeb7765560ca945
Madhura <mgadgil09@gmail.com>
2016-05-04 16:44:03 -0400

    adding RF

--- SentimentAnalysis_NLTK_NB/src/RF.py
+++ SentimentAnalysis_NLTK_NB/src/RF.py
@@ -3,6 +3,8 @@ from fileinput import input
 from pyspark.mllib.regression import LabeledPoint
 from glob import glob
 from pyspark.mllib.util import MLUtils
+from pyspark.mllib.tree import RandomForest, RandomForestModel
+from pyspark.mllib.util import MLUtils
 #>>> examples = [LabeledPoint(1.1, Vectors.sparse(3, [(0, 1.23), (2, 4.56)])),                         LabeledPoint(0.0, Vectors.dense([1.01, 2.02, 3.03]))]
 #>>> tempFile = NamedTemporaryFile(delete=True)
 #>>> tempFile.close()
@@ -18,13 +20,17 @@ from pyspark import SparkContext
 sc=SparkContext("local","dd")
 train = sc.parallelize(open("/home/madhura/ML_Spring16/MLProject/data/OriginalTraining.txt").read().splitlines()).map(lambda x: x.split(","))
 trainlabels = train.map(lambda(a,b): int(b))
-traintf = HashingTF(numFeatures=500).transform(train.map(lambda(a,b): a.split()))
+traintf = HashingTF().transform(train.map(lambda(a,b): a.split()))
 trainidf = IDF().fit(traintf)
 traintfidf = trainidf.transform(traintf)
 #densetrain = traintfidf.map(lambda x: pyspark.mllib.linalg.DenseVector(x.toArray()))
 #zippeddata = trainlabels.zip(densetrain)
 #new = zippeddata.map(lambda (a,vec) : (a,vec.toArray()))
 training = trainlabels.zip(traintfidf).map(lambda x : LabeledPoint(x[0], x[1]))
-tempFile = NamedTemporaryFile(delete=True)
-tempFile.close()
-MLUtils.saveAsLibSVMFile(sc.parallelize(training),tempFile.name)
+MLUtils.saveAsLibSVMFile(training.coalesce(1),"/home/madhura/ML_Spring16/MLProject/data/libsvmfile")
+data = MLUtils.loadLibSVMFile(sc, "/home/madhura/ML_Spring16/MLProject/data/libsvmfile/part-00000")
+(trainingData, testData) = data.randomSplit([0.7, 0.3])
+model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
+                                     numTrees=3, featureSubsetStrategy="auto",
+                                     impurity='gini', maxDepth=4, maxBins=32)
+predictions = model.predict(testData.map(lambda x: x.features))                                     
