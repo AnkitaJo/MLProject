0fb8aebb9c1975e85df9213b4234cd6a3973cc34
nakte <ankita.joshi1591@gmail.com>
2016-05-03 23:14:08 -0400

    hastags analysis done

--- readjson.py
+++ readjson.py
@@ -13,17 +13,16 @@ for i in range(0, len(tdata)):
 	result1 = tdata[i]["tweet_text"]
 	#removing the URLs
 	result2 = re.sub(r"http\S+", "", result1)
-	#removing the punctuation marks
-        result3 = ''.join(c for c in result2 if c not in punctuation)	
-	#remove other non-ascii stuff
-	result4 = re.sub(r'[^\x00-\x7f]',r'', result3)
+	result3 = re.sub(r'^\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*\(\s*\)#','',result2)
+#	result4 = "".join(i for i in result3 if ord(i)<128 or ord(i) == unicode('#'))
 	#remove extra spaces or lines
-	result5 = re.sub('\s+', ' ',result4).strip()
+	result4 = re.sub('\s+', ' ',result3).strip()
 	#remove "RT"
-	result6 = re.sub("RT",'',result5).strip()
+	result5 = re.sub("RT",'',result4).strip()
 	#make the final string 	
-	line = tdata[i]["user_id"] +","+result6+"\n"
+	line = tdata[i]["user_id"] +","+result5+"\n"
 	#write the output #id,tweet to the file
-	with codecs.open('file.txt', 'a', encoding = 'utf-8') as out:
+	with codecs.open('file1.txt', 'a', encoding = 'utf-8') as out:
 		out.write(line)
 		out.close
+
