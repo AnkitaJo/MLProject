e72801fca39113f1eff1ca01d870e59afa75b898
nakte <ankita.joshi1591@gmail.com>
2016-05-04 19:46:47 -0400

    working

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -11,41 +11,34 @@ from pyspark.ml.feature import HashingTF,IDF,Tokenizer
 from pyspark import SparkContext
 
 
-
 sc=SparkContext("local","dd")
 sqlContext = SQLContext(sc)
 
-lines = sc.textFile("/home/ankita/MLProject/data/OriginalTraining.txt")
+lines = sc.textFile("/home/ankita/MLProject/data/labeled1.txt")
+
 parts = lines.map(lambda l: l.split(","))
-#f = parts.map(lambda p: Row(tweet=p[0], label=int(float(p[1]))))
 indexedTweets = parts.zipWithIndex()
-f = indexedTweets.map(lambda (a,b): Row(index=int(b),tweet=a[0],label=int(float(a[1]))))
+f = indexedTweets.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][0], label=int(float(p[0][1]))))
+#f = parts.map(lambda p: Row(tweet=p[0],label=int(p[1])))
+
 schemaTweets = sqlContext.createDataFrame(f)
+
 schemaTweets.registerTempTable("data")
-
-
 
 tokenizer = Tokenizer(inputCol="tweet", outputCol="words")
 wordsData = tokenizer.transform(schemaTweets)
+
 hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures")
 featurizedData = hashingTF.transform(wordsData)
+
+
 idf = IDF(inputCol="rawFeatures", outputCol="features")
+
+
+
 idfModel = idf.fit(featurizedData)
 rescaledData = idfModel.transform(featurizedData)
-#for features_label in rescaledData.select("tweet","features", "label").take(3):
- #   print(features_label)
-rescaledData.printSchema()
 
-"""
-# Build the model
-model = SVMWithSGD.train(parsedData, iterations=100)
+for features_label in rescaledData.select("tweet","features", "label").take(3):
+    print(features_label)
 
-# Evaluating the model on training data
-labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
-print("Training Error = " + str(trainErr))
-
-# Save and load model
-model.save(sc, "myModelPath")
-sameModel = SVMModel.load(sc, "myModelPath")
-"""
