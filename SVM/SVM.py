c9debd474ef280ac081448672038ca54634bc3dc
nakte <ankita.joshi1591@gmail.com>
2016-05-04 17:11:50 -0400

    tf-idf working

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -1,13 +1,43 @@
 from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark import SparkContext
+from fileinput import input
+from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.regression import LabeledPoint
+from glob import glob
+from pyspark.mllib.util import MLUtils
+from pyspark.sql import SQLContext, Row
+from pyspark.sql.types import *
+from pyspark.ml.feature import HashingTF,IDF,Tokenizer
+from pyspark import SparkContext
 
-# Load and parse the data
-def parsePoint(line):
-    values = [float(x) for x in line.split(',')]
-    return LabeledPoint(values[1], values[0:])
 
-data = sc.textFile("../data/OriginalTraining.txt")
-parsedData = data.map(parsePoint)
+
+sc=SparkContext("local","dd")
+sqlContext = SQLContext(sc)
+
+lines = sc.textFile("/home/ankita/MLProject/data/OriginalTraining.txt")
+parts = lines.map(lambda l: l.split(","))
+f = parts.map(lambda p: Row(tweet=p[0], label=int(float(p[1]))))
+
+schemaTweets = sqlContext.createDataFrame(f)
+schemaTweets.registerTempTable("data")
+
+
+
+tokenizer = Tokenizer(inputCol="tweet", outputCol="words")
+wordsData = tokenizer.transform(schemaTweets)
+hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures")
+featurizedData = hashingTF.transform(wordsData)
+idf = IDF(inputCol="rawFeatures", outputCol="features")
+idfModel = idf.fit(featurizedData)
+rescaledData = idfModel.transform(featurizedData)
+#for features_label in rescaledData.select("tweet","features", "label").take(3):
+ #   print(features_label)
+rescaledData.printSchema()
+
+
+"""
+
 
 # Build the model
 model = SVMWithSGD.train(parsedData, iterations=100)
@@ -20,3 +50,4 @@ print("Training Error = " + str(trainErr))
 # Save and load model
 model.save(sc, "myModelPath")
 sameModel = SVMModel.load(sc, "myModelPath")
+"""
