bcf8e33457bf354f9645c95ab61195db7616305d
nakte <ankita.joshi1591@gmail.com>
2016-05-04 20:22:08 -0400

    working SVM trained with acc = 0.06

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -14,11 +14,11 @@ from pyspark import SparkContext
 sc=SparkContext("local","dd")
 sqlContext = SQLContext(sc)
 
-lines = sc.textFile("/home/ankita/MLProject/data/labeled1.txt")
+lines = sc.textFile("/home/ankita/MLProject/data/OriginalTraining.txt")
 
 parts = lines.map(lambda l: l.split(","))
 indexedTweets = parts.zipWithIndex()
-f = indexedTweets.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][0], label=int(float(p[0][1]))))
+f = indexedTweets.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][0], label= int(float(p[0][1]))))
 #f = parts.map(lambda p: Row(tweet=p[0],label=int(p[1])))
 
 schemaTweets = sqlContext.createDataFrame(f)
@@ -39,6 +39,10 @@ idf = IDF(inputCol="rawFeatures", outputCol="features")
 idfModel = idf.fit(featurizedData)
 rescaledData = idfModel.transform(featurizedData)
 
-for features_label in rescaledData.select("tweet","features", "label").take(3):
-    print(features_label)
+wordsvectors = rescaledData["label","features"].map(lambda row: LabeledPoint(row[0], row[1]))
+model = SVMWithSGD.train(wordsvectors, iterations=100)
 
+labelsAndPreds = wordsvectors.map(lambda p: (p.label, model.predict(p.features)))
+trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(wordsvectors.count())
+print("Training Error = " + str(trainErr))
+model.save(sc, "/home/ankita/MLProject/SVM/myModelPath")
