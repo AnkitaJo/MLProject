1f3d5641ac989614de47b1b9235c41a134785f42
AnkitaJo <ankita.joshi1591@gmail.com>
2016-05-05 14:28:59 -0400

    Changes in SVM and added module of logistic regression

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -18,10 +18,18 @@ lines = sc.textFile("/home/ankita/MLProject/data/OriginalTraining.txt")
 
 parts = lines.map(lambda l: l.split(","))
 indexedTweets = parts.zipWithIndex()
-f = indexedTweets.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][0], label= int(float(p[0][1]))))
+trainingCount=parts.count()
+f = indexedTweets.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][0], label= int(float(p[0][1])), training=1))
 #f = parts.map(lambda p: Row(tweet=p[0],label=int(p[1])))
 
-schemaTweets = sqlContext.createDataFrame(f)
+linest = sc.textFile("/home/ankita/MLProject/SVM/GroundTruth.txt")
+
+partst = linest.map(lambda l: l.split(","))
+indexedTweetst = partst.zipWithIndex().map(lambda (a,b): (a,b+trainingCount))
+ft = indexedTweetst.map(lambda p: Row(tindex=int(p[1]),tweet=p[0][1], label= int(float(p[0][0])),training=0))
+alldata = f.union(ft)
+
+schemaTweets = sqlContext.createDataFrame(alldata)
 
 schemaTweets.registerTempTable("data")
 
@@ -38,11 +46,24 @@ idf = IDF(inputCol="rawFeatures", outputCol="features")
 
 idfModel = idf.fit(featurizedData)
 rescaledData = idfModel.transform(featurizedData)
-
-wordsvectors = rescaledData["label","features"].map(lambda row: LabeledPoint(row[0], row[1]))
+#rescaledData.collect()
+wordsvectors = rescaledData.filter(rescaledData.training==1)["label","features"].map(lambda row: LabeledPoint(row[0], row[1]))
 model = SVMWithSGD.train(wordsvectors, iterations=100)
 
 labelsAndPreds = wordsvectors.map(lambda p: (p.label, model.predict(p.features)))
 trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(wordsvectors.count())
 print("Training Error = " + str(trainErr))
-model.save(sc, "/home/ankita/MLProject/SVM/myModelPath")
+#model.save(sc, "/home/ankita/MLProject/SVM/myModelPath3")
+resct=rescaledData.filter(rescaledData.training==0)
+labelsAndPreds =resct.map(lambda p: (p.label, model.predict(p.features)))
+trueneg = labelsAndPreds.filter(lambda (v,p): v==0 and p==0).count() 
+falseneg = labelsAndPreds.filter(lambda (v,p): v==1 and p==0).count() 
+truepos = labelsAndPreds.filter(lambda (v,p): v==1 and p==1).count()
+falsepos = labelsAndPreds.filter(lambda (v,p): v==0 and p==1).count()
+
+print("false negative:", falseneg)
+print("false positive:", falsepos)
+print("true negative:", trueneg)
+print("true positive:", truepos)
+print("Fscore:", float(2*truepos)/(2*truepos + falsepos + falseneg))
+print("Test Error = ", float(falseneg + falsepos)/resct.count())
